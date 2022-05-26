e815464ed4ed02eb6eaac2ebbfcc29870c46bf2c
shlokrane <105918707+shlokrane@users.noreply.github.com>
2022-05-26 19:55:00 +0530

    Create Question 5.py

---
+++ Assigntment Questions/Set 5/Question 5.py
@@ -0,0 +1,33 @@
+list_of_marks=(12,18,25,24,2,5,18,20,20,21)
+def find_more_than_average():
+    global list_of_marks
+    marks=0
+    count=0
+    for x in list_of_marks:
+        marks+=x
+    average=marks/len(list_of_marks)
+    for x in list_of_marks:
+        if x>average:
+            count+=1
+    percentage=(count*100)/len(list_of_marks)
+    return percentage
+    
+def sort_marks():
+    global list_of_marks
+    list_of_marks=sorted(list_of_marks)
+    return list_of_marks
+    
+def generate_frequency():
+    freq=[]
+    global list_of_marks
+    for x in range(26):
+        count=0
+        for y in list_of_marks:
+            if x == y:
+                count+=1
+        freq.append(count)
+    return  freq
+    
+print(find_more_than_average())
+print(generate_frequency())
+print(sort_marks())
